75d67a576ba2691f61acf6dbfc610d8e4860ac42
yangsong <18183263596@163.com>
2020-06-11 13:55:56 +0800

    第一次建springcloud

---
+++ cloud-provider-payment8001/src/main/java/com/ys/springcloud/PaymentMain8001.java
@@ -0,0 +1,19 @@
+package com.ys.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述:
+ *
+ * @version jdk1.8
+ * @ClassName PaymentMain8001
+ * @Author yangsong
+ * @Date 2020/6/11 13:38
+ **/
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
